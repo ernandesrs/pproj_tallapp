e0e3f7a0a51923a3ec5d8f57df02303823725de7
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-26 13:17:19 -0400

    Tallstack UI config and Tailwind Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 import forms from '@tailwindcss/forms'
 
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 export default {
     presets: [
@@ -13,7 +15,52 @@ export default {
         './vendor/tallstackui/tallstackui/src/**/*.php',
     ],
     theme: {
-        extend: {},
+        extend: {
+            ...colors,
+
+            'primary': {
+                DEFAULT: colors.indigo[400],
+                '50': colors.indigo[50],
+                '100': colors.indigo[100],
+                '200': colors.indigo[200],
+                '300': colors.indigo[300],
+                '400': colors.indigo[400],
+                '500': colors.indigo[500],
+                '600': colors.indigo[600],
+                '700': colors.indigo[700],
+                '800': colors.indigo[800],
+                '900': colors.indigo[900],
+                '950': colors.indigo[950],
+            },
+            'secondary': {
+                DEFAULT: colors.sky[400],
+                '50': colors.sky[50],
+                '100': colors.sky[100],
+                '200': colors.sky[200],
+                '300': colors.sky[300],
+                '400': colors.sky[400],
+                '500': colors.sky[500],
+                '600': colors.sky[600],
+                '700': colors.sky[700],
+                '800': colors.sky[800],
+                '900': colors.sky[900],
+                '950': colors.sky[950],
+            },
+            'dark': {
+                DEFAULT: colors.zinc[700],
+                '50': colors.zinc[50],
+                '100': colors.zinc[100],
+                '200': colors.zinc[200],
+                '300': colors.zinc[300],
+                '400': colors.zinc[400],
+                '500': colors.zinc[500],
+                '600': colors.zinc[600],
+                '700': colors.zinc[700],
+                '800': colors.zinc[800],
+                '900': colors.zinc[900],
+                '950': colors.zinc[950],
+            }
+        },
     },
     plugins: [
         forms
