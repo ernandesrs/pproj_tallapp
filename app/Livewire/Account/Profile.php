62ea51c2c8eb80a5e38c948c5c79259b1f12f50a
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-27 15:43:59 -0400

    Using the custom content card and upload profile avatar

--- app/Livewire/Account/Profile.php
+++ app/Livewire/Account/Profile.php
@@ -3,9 +3,38 @@
 namespace App\Livewire\Account;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Illuminate\Http\UploadedFile;
 
 class Profile extends Component
 {
+    use WithFileUploads;
+
+    /**
+     * Auth user instance
+     * @var \App\Models\User
+     */
+    public \App\Models\User $user;
+
+    /**
+     * Avatar
+     * @var null|\Livewire\Features\SupportFileUploads\TemporaryUploadedFile
+     */
+    public null|\Livewire\Features\SupportFileUploads\TemporaryUploadedFile $avatar = null;
+
+    /**
+     * Mount
+     * @return void
+     */
+    public function mount()
+    {
+        $this->user = \Auth::user();
+    }
+
+    /**
+     * Render view
+     * @return mixed
+     */
     public function render()
     {
         return view('livewire..account.profile')
@@ -14,5 +43,57 @@ class Profile extends Component
                     'title' => 'Meu perfil'
                 ]
             ]);
+    }
+
+    /**
+     * Avatar uplaod
+     * @return void
+     */
+    public function saveAvatar()
+    {
+        $path = $this->avatar->store('avatars', ['disk' => 'public']);
+        if ($path) {
+            $this->user->avatar = $path;
+            $this->user->save();
+        }
+    }
+
+    /**
+     * Delete temporary uploaded avatar
+     * @param array $content
+     * @return void
+     */
+    public function deleteAvatar(array $content)
+    {
+        /* the $content contains:
+            [
+                'temporary_name',
+                'real_name',
+                'extension',
+                'size',
+                'path',
+                'url',
+            ]
+        */
+
+        if (!$this->avatar) {
+            return;
+        }
+
+        $files = \Illuminate\Support\Arr::wrap($this->avatar);
+
+        /** @var UploadedFile $file */
+        $file = collect($files)->filter(fn(UploadedFile $item) => $item->getFilename() === $content['temporary_name'])->first();
+
+        // 1. Here we delete the file. Even if we have a error here, we simply
+        // ignore it because as long as the file is not persisted, it is
+        // temporary and will be deleted at some point if there is a failure here.
+        rescue(fn() => $file->delete(), report: false);
+
+        $collect = collect($files)->filter(fn(UploadedFile $item) => $item->getFilename() !== $content['temporary_name']);
+
+        // 2. We guarantee restore of remaining files regardless of upload
+        // type, whether you are dealing with multiple or single uploads
+        $this->avatar = is_array($this->avatar) ? $collect->toArray() : $collect->first();
     }
 }
--- resources/views/livewire/account/profile.blade.php
+++ resources/views/livewire/account/profile.blade.php
@@ -1,16 +1,55 @@
 <x-admin.page
     title="Meu perfil">
 
-    <div class="col-span-12 lg:col-span-4">
-        <x-card>
-            Ali
-        </x-card>
+    <div class="col-span-12 md:col-span-4 grid gap-6">
+        <x-admin.content-card
+            icon="user-circle"
+            title="Seu avatar"
+            class="col-span-12">
+            <div class="flex flex-col gap-y-3">
+                <div class="flex-1 flex gap-5">
+                    @if (!empty(\Auth::user()->avatar))
+                        <x-avatar
+                            :image="\Str::startsWith(\Auth::user()->avatar, ['http://', 'https://'])
+                                ? \Auth::user()->avatar
+                                : \Storage::url(\Auth::user()->avatar)" lg />
+                    @else
+                        <x-avatar text="{{ \Auth::user()->username[0] }}" lg />
+                    @endif
+
+                    <div class="flex-1">
+                        <x-upload wire:model='avatar' label="Novo avatar" delete delete-method="deleteAvatar"
+                            accept="image/*" />
+                    </div>
+                </div>
+
+                @if ($this->avatar)
+                    <div class="flex-1 flex justify-center">
+                        <x-button wire:click='saveAvatar' text="Atualizar" />
+                    </div>
+                @endif
+            </div>
+        </x-admin.content-card>
+
+        <x-admin.content-card
+            icon="list-letters"
+            title="Detalhes"
+            class="col-span-12">
+            Some account details
+        </x-admin.content-card>
     </div>
 
-    <div class="col-span-12 lg:col-span-8">
-        <x-card>
-            Ali
-        </x-card>
+    <div class="col-span-12 md:col-span-8 grid gap-6">
+        <x-admin.content-card
+            icon="id"
+            title="Profile data"
+            class="col-span-12">
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi rerum laborum saepe cum consequuntur
+                eligendi accusamus perferendis explicabo commodi non quibusdam maiores, nobis, mollitia expedita ipsa
+                dolorem ut dolores quae?
+            </p>
+        </x-admin.content-card>
     </div>
 
 </x-admin.page>
