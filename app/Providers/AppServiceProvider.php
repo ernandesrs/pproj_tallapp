1db6e94d93cd4bb9a2787255629ec2f4c0548029
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-27 13:17:32 -0400

    Customizing card component(from Tallstack UI) colors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use TallStackUi\Facades\TallStackUi;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        TallStackUi::personalize()
+            ->card()
+            ->block('wrapper.second')
+            ->replace('dark:bg-dark-700', 'dark:bg-dark-800');
     }
 }
--- resources/views/livewire/account/profile.blade.php
+++ resources/views/livewire/account/profile.blade.php
@@ -1,4 +1,16 @@
 <x-admin.page
     title="Meu perfil">
 
+    <div class="col-span-12 lg:col-span-4">
+        <x-card>
+            Ali
+        </x-card>
+    </div>
+
+    <div class="col-span-12 lg:col-span-8">
+        <x-card>
+            Ali
+        </x-card>
+    </div>
+
 </x-admin.page>
