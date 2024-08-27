0802fd26bf1638062eaeb9a901bdae3a9097a0a8
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-27 10:50:31 -0400

    Updating overivew card componentj

--- resources/views/components/admin/overview-card.blade.php
+++ resources/views/components/admin/overview-card.blade.php
@@ -2,20 +2,46 @@
     'icon' => 'app',
     'title' => '',
     'value' => 0,
+
+    /**
+     * Footer links
+     */
+    'links' => [
+        /**
+         * Each item can be:
+         * [
+         *      'text' => 'Link text',
+         *      'href' => 'Link',
+         *      'external' => false,
+         *      'icon' => 'link'
+         * ]
+         */
+    ],
 ])
 
 <div {{ $attributes }}>
     <x-card>
-        <div class="flex gap-4 items-center">
+        <div class="flex gap-4 items-center text-zinc-600 dark:text-zinc-300">
             <x-icon name="{{ $icon }}" />
             <div>
                 <h3 class="text-xl">
                     {{ $title }}
                 </h3>
-                <h3 class="text-4xl">
+                <h3 class="text-4xl text-zinc-700 dark:text-zinc-100">
                     {{ $value }}
                 </h3>
             </div>
         </div>
+
+        @if (count($links))
+            <x-slot:footer>
+                @foreach ($links as $link)
+                    @php
+                        $link = (object) $link;
+                    @endphp
+                    <x-link :href="$link->href" :text="$link->text" :icon="$link->icon ?? ''" position="left" :navigate="!($link->external ?? false)" />
+                @endforeach
+            </x-slot:footer>
+        @endif
     </x-card>
 </div>
--- resources/views/livewire/admin/overview.blade.php
+++ resources/views/livewire/admin/overview.blade.php
@@ -6,19 +6,39 @@
         class="col-span-12 sm:col-span-6 lg:col-span-4"
         icon="users-group"
         title="Usuários"
-        :value="\App\Models\User::count()" />
-
+        :value="\App\Models\User::count()"
+        :links="[
+            [
+                'text' => 'Gerenciar',
+                'icon' => 'settings',
+                'href' => route('admin.users.index'),
+            ],
+        ]" />
 
     <x-admin.overview-card
         class="col-span-12 sm:col-span-6 lg:col-span-4"
         icon="shield-half"
         title="Cargos"
-        :value="\App\Models\Role::count()" />
+        :value="\App\Models\Role::count()"
+        :links="[
+            [
+                'text' => 'Gerenciar',
+                'icon' => 'settings',
+                'href' => route('admin.roles.index'),
+            ],
+        ]" />
 
     <x-admin.overview-card
         class="col-span-12 sm:col-span-6 lg:col-span-4"
-        icon="shield-check"
-        title="Permissões"
-        :value="\App\Models\Permission::count()" />
+        icon="settings"
+        title="Dolorem card"
+        :value="198"
+        :links="[
+            [
+                'text' => 'Lorem link',
+                'icon' => 'settings',
+                'href' => '#',
+            ],
+        ]" />
 
 </x-admin.page>
