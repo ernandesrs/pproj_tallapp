73abe058c39c9e9ef5f0e05326b73739fba43af4
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-29 22:41:42 -0400

    Creating roles

--- app/Livewire/Admin/Roles/Create.php
+++ app/Livewire/Admin/Roles/Create.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire\Admin\Roles;
 
+use App\Models\Role;
+use App\Services\RoleService;
 use Livewire\Component;
+use TallStackUi\Traits\Interactions;
 
 class Create extends Component
 {
+    use Interactions;
+
+    public string $name = '';
+
+    /**
+     * Render view
+     * @return mixed
+     */
     public function render()
     {
         return view('livewire..admin.roles.create')
@@ -14,5 +25,32 @@ class Create extends Component
                     'title' => 'Novo cargo'
                 ]
             ]);
+    }
+
+    /**
+     * Save role
+     * @return void
+     */
+    public function save()
+    {
+        $this->authorize('create', Role::class);
+
+        $created = RoleService::create(
+            $this->validate(RoleService::rules()::creationRules())
+        );
+
+        if (!$created) {
+            $this->toast()
+                ->error('Erro!', 'Houve um erro inesperado ao tentar criar novo cargo.')
+                ->send();
+            return;
+        }
+
+        $this->toast()
+            ->success('Criado!', 'Novo cargo criado com sucesso.')
+            ->flash()
+            ->send();
+
+        return $this->redirect(route('admin.roles.edit', ['role' => $created->id]));
     }
 }
--- resources/views/livewire/admin/roles/create.blade.php
+++ resources/views/livewire/admin/roles/create.blade.php
@@ -1,12 +1,17 @@
 <x-admin.page
     title="Novo cargo">
 
-    <div class="col-span-12 grid grid-cols-12 gap-6">
-        <div class="col-span-12">
-            <x-input wire:model="name" label="Name do cargo *" />
+    <x-admin.content-card class="col-span-12">
+        <div class="grid grid-cols-12 gap-6">
+            <div class="col-span-12">
+                <x-input wire:model="name" label="Name do cargo *" />
+            </div>
+
+            <div class="col-span-12 flex justify-center">
+                <x-button wire:target="save" wire:click="save" text="Criar cargo" icon="check"
+                    loading />
+            </div>
         </div>
-        <div class="col-span-12">
-        </div>
-    </div>
+    </x-admin.content-card>
 
 </x-admin.page>
