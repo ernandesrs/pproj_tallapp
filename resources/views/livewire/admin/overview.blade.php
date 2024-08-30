5acfed6f3e40639ac098cd0ca8adf0eb39c69f1a
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-30 10:08:43 -0400

    Applying overview card authorization

--- resources/views/livewire/admin/overview.blade.php
+++ resources/views/livewire/admin/overview.blade.php
@@ -2,29 +2,72 @@
     hide-header
     title="Visão geral">
 
+    @can('viewAny', \App\Models\User::class)
+        <x-admin.overview-card
+            class="col-span-12 sm:col-span-6 lg:col-span-4"
+            icon="users-group"
+            title="Usuários"
+            :value="\App\Models\User::count()"
+            :links="[
+                [
+                    'text' => 'Gerenciar',
+                    'icon' => 'settings',
+                    'href' => route('admin.users.index'),
+                ],
+            ]" />
+    @endcan
+
+    @can('viewAny', \App\Models\Role::class)
+        <x-admin.overview-card
+            class="col-span-12 sm:col-span-6 lg:col-span-4"
+            icon="shield-half"
+            title="Cargos"
+            :value="\App\Models\Role::count()"
+            :links="[
+                [
+                    'text' => 'Gerenciar',
+                    'icon' => 'settings',
+                    'href' => route('admin.roles.index'),
+                ],
+            ]" />
+    @endcan
+
     <x-admin.overview-card
         class="col-span-12 sm:col-span-6 lg:col-span-4"
-        icon="users-group"
-        title="Usuários"
-        :value="\App\Models\User::count()"
+        icon="settings"
+        title="Dolorem card"
+        :value="198"
         :links="[
             [
-                'text' => 'Gerenciar',
+                'text' => 'Lorem link',
                 'icon' => 'settings',
-                'href' => route('admin.users.index'),
+                'href' => '#',
             ],
         ]" />
 
     <x-admin.overview-card
         class="col-span-12 sm:col-span-6 lg:col-span-4"
-        icon="shield-half"
-        title="Cargos"
-        :value="\App\Models\Role::count()"
+        icon="settings"
+        title="Dolorem card"
+        :value="198"
         :links="[
             [
-                'text' => 'Gerenciar',
+                'text' => 'Lorem link',
                 'icon' => 'settings',
-                'href' => route('admin.roles.index'),
+                'href' => '#',
+            ],
+        ]" />
+
+    <x-admin.overview-card
+        class="col-span-12 sm:col-span-6 lg:col-span-4"
+        icon="settings"
+        title="Dolorem card"
+        :value="198"
+        :links="[
+            [
+                'text' => 'Lorem link',
+                'icon' => 'settings',
+                'href' => '#',
             ],
         ]" />
 
