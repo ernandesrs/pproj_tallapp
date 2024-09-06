984b2bbf9b04710ae38ecaa97d4dfa2088531545
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-09-06 11:21:46 -0400

    Custom list table: no results

--- resources/views/components/admin/list/custom.blade.php
+++ resources/views/components/admin/list/custom.blade.php
@@ -5,19 +5,19 @@
     'actionDelete' => false,
 ])
 
-@if ($rows->count())
-    <div class="border dark:border-zinc-700 rounded-md w-full border-1 border-zinc-300 overflow-x-auto">
-        <table
-            class="table-auto w-full bg-zinc-100 dark:bg-zinc-700 text-left text-zinc-600 rounded-md overflow-hidden dark:text-zinc-200">
-            <thead>
-                <tr
-                    class="bg-zinc-100 uppercase border-b text-sm font-medium border-b-zinc-200 dark:bg-zinc-700 dark:border-b-zinc-600">
-                    @foreach ($headers as $header)
-                        <td class="px-3 py-4">{{ $header['label'] }}</td>
-                    @endforeach
-                </tr>
-            </thead>
-            <tbody class="text-zinc-400">
+<div class="border dark:border-zinc-700 rounded-md w-full border-1 border-zinc-300 overflow-x-auto">
+    <table
+        class="table-auto w-full bg-zinc-100 dark:bg-zinc-700 text-left text-zinc-600 rounded-md overflow-hidden dark:text-zinc-200">
+        <thead>
+            <tr
+                class="bg-zinc-100 uppercase border-b text-sm font-medium border-b-zinc-200 dark:bg-zinc-700 dark:border-b-zinc-600">
+                @foreach ($headers as $header)
+                    <td class="px-3 py-4">{{ $header['label'] }}</td>
+                @endforeach
+            </tr>
+        </thead>
+        <tbody class="text-zinc-400">
+            @if ($rows->count())
                 @foreach ($rows as $row)
                     <tr class="border-b border-b-zinc-200 bg-zinc-50 dark:border-b-zinc-700 dark:bg-zinc-800">
                         @foreach ($headers as $header)
@@ -42,13 +42,19 @@
                         @endforeach
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
-    </div>
+            @else
+                <tr class="bg-zinc-50 dark:bg-zinc-800">
+                    <td class="px-3 py-2 text-center text-lg" colspan="{{ count($headers) }}">
+                        Sem resultados
+                    </td>
+                </tr>
+            @endif
+        </tbody>
+    </table>
+</div>
 
+@if ($rows->count())
     <div class="mt-3">
         {{ $rows->onEachSide(0)->links() }}
     </div>
-@else
-    Sem items
 @endif
